4e27180843790c7f973f6c4f14ac2a4e4f7b403b
Gabriel Cornejo <gcornejo441@gmail.com>
2024-05-27 11:12:52 -0400

    bug-fix: failed prod build, removed 'React' declaration

--- src/components/Timer/components/Timer.tsx
+++ src/components/Timer/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { TimePadder } from "../../../utilities/helpers";
 import ButtonPanel from "../../ButtonPanel/ButtonPanel";
 import TimerDisplay from "./TimerDisplay";
