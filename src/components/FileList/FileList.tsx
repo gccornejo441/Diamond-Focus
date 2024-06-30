7654bc2bbedc66dd6dc6bcab534c0a92ea59413f
Gabriel Cornejo <gcornejo441@gmail.com>
2024-06-30 08:06:09 -0400

    bug-fix: correcting build error

--- src/components/FileList/FileList.tsx
+++ src/components/FileList/FileList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 import { useAuth } from "@utilities/AuthContext";
 import { toast } from "react-toastify";
